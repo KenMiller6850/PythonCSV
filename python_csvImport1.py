1bf7df21701f92295c0864a3133e6948a633d677
KenMiller6850 <102335917+KenMiller6850@users.noreply.github.com>
2022-05-03 21:37:47 -0400

    Add files via upload

---
+++ python_csvImport1.py
@@ -0,0 +1,29 @@
+# To insert data frame into MS SQL database without iterate the data-frame
+
+import pandas
+from pathlib import Path
+from os import chdir
+from sqlalchemy import create_engine, MetaData, Table, select,text
+from ReadConfig2 import getSQLCONFIG
+
+ 
+filePath = Path(__file__).resolve().parent / "FlagstaffWeatherData.csv"
+configFilePath = Path(__file__).resolve().parent / "config.ini"
+
+
+c = getSQLCONFIG(configFilePath)
+
+#params = ("DRIVER={SQL Server};SERVER=dev_151.sql.caresource.corp\dev_151;DATABASE=InternalAudit;Trusted_Connection=yes")
+params = ("DRIVER={SQL Server};SERVER=" + c[1] + ";DATABASE=" + c[0] +";Trusted_Connection=yes")
+
+df = pandas.read_csv(filePath,parse_dates=['DATE'])
+df2 = df.fillna(-999)
+
+try:
+
+  engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
+  engine.connect()
+  df2.to_sql(name='WeatherData',con=engine, index=False, if_exists='replace')
+  print('CSV file transferred') 
+ except:
+  print('Database connection error') 
